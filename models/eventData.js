835e30ea4411c5f9c6d149ff984daf54ea6cfd02
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-02-02 23:44:58 +0800

    [Event API] Implement GET /v1/event

--- models/eventData.js
+++ models/eventData.js
@@ -2,13 +2,19 @@ var pool = require(__dirname + '/dbConnectionPool.js');
 
 function EventData() {}
 
+var CONFIRMED = 'CONFIRMED';
+
 EventData.prototype.readAll = function() {
     return pool.query('SELECT * FROM eventdata');
 };
 
-EventData.prototype.read = function(txHash) {
-    return pool.query('SELECT * FROM eventdata WHERE txHash = $1', [txHash]);
-};
+EventData.prototype.readByTxHash = function(txHash) {
+	return pool.query('SELECT * FROM eventData WHERE transactionHash = ('
+		+ ' SELECT transactionHash FROM transactionData '
+		+ ' WHERE txHash = $1 ' 
+		+ ' AND status = $2)', 
+		[txHash, CONFIRMED]);
+}
 
 EventData.prototype.create = function(entity) {
     return pool.query('INSERT INTO eventdata '
@@ -33,3 +39,7 @@ EventData.prototype.delete = function() {
 };
 
 exports = module.exports = new EventData();
+SELECT * FROM eventData WHERE transactionHash = (
+SELECT transactionHash FROM transactionData 
+WHERE txHash = '6590fa52fa76f967656340b874bc9ca09733c2fddea9886210ebcbbceee04b35' 
+AND status = 'CONFIRMED');
