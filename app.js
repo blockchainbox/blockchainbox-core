d8ee316ef5686cf930c5748ef3bb248bf3491980
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-05-28 21:53:57 +0800

    [Core] Add CORS for Express

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 // TODO 這邊要加上 whitelist 來控管誰可以對這個 enode 下指令
@@ -23,6 +24,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
